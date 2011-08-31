8586807fb3a9da682f3753411051b129ca269f2c
Jared Blitzstein <jared@blitzstein.net>
2011-08-31 16:22:34 -0400

    Refactord variable names

--- main.rb
+++ main.rb
@@ -2,27 +2,16 @@ require 'sinatra'
 require 'mechanize'
 require File.join(File.dirname(__FILE__), 'navi')
 
-
-ACCOUNT = "blitzsteinj:Katie1980"
 get '/' do
   agent = Mechanize.new
-  #page = agent.get('http://crowd.tools.us.gspt.net/crowd/console/login.action')
-  #login_form = page.form("login")
-  #login_form.j_username = j_username
-  #login_form.j_password = j_password
-
-  #login_result = agent.submit(login_form, login_form.buttons.first)
 
   if (params[:username] == nil || params[:username].empty?)
     @user_tables = []
   else
     ln = LoginNav.new(agent)
-    r = ln.login
-    s = ln.search("#{params[:username]}")
-    @user_tables = ln.parse_users(s)
+    perform_login_results = ln.login
+    perform_search_results = ln.search("#{params[:username]}")
+    @user_tables = ln.parse_users(perform_search_results)
   end
-
-
   erb :index 
-
 end
