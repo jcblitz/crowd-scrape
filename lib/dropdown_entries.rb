9b8fb8215609199f58679798740bfa0ffa0ddfed
Jared Blitzstein <jared@blitzstein.net>
2011-09-01 10:07:52 -0400

    Removed specific entries

--- lib/dropdown_entries.rb
+++ lib/dropdown_entries.rb
@@ -4,10 +4,8 @@ module VariablesModule
   # DROPDOWNS << DropdownEntry.new("New York Office", 1)
   # This is saying select the 1st dropdown position and call it "New York Office"
   # Create an entry for each directory you want to search. The first field is a display label when showing results and the second numeric field is the position in the dropdown menu. Do not forget 0 is actually the first entry.
-  DROPDOWNS << DropdownEntry.new("GSI", 5)
-  DROPDOWNS << DropdownEntry.new("ISH", 6)
-  DROPDOWNS << DropdownEntry.new("SysIQ", 9)
+  DROPDOWNS << DropdownEntry.new("New York Office", 1)
 
   #This is the base url for your crowd installation. Something like http://crowd.mycompany.com
-  BASE_URL = "http://crowd.tools.us.gspt.net"
+  BASE_URL = ""
 end
